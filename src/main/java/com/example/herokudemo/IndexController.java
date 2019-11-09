3a7cb7612d86297fb73d2d6e45e100c3c808c938
jvquiles <jvquiles@gmail.com>
2019-11-09 14:41:30 +0100

    More funcitions

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -7,7 +7,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
+    public String Index() {
         return "Hello there! I'm running.";
+    }
+    
+    @GetMapping("/coment/add")
+    public String AddComment() {
+        return "Comentario añadido.";
+    }
+    
+    @GetMapping("/coment/delete")
+    public String DeleteComment() {
+        return "Comentario eliminado.";
+    }
+    
+    @GetMapping("/coment")
+    public String Comment() {
+        return "Listar comentarios.";
     }
 }
