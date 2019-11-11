47bf05bd0b1f9ae0453bff2504e3fb2db9129818
jvquiles <jvquiles@gmail.com>
2019-11-11 23:10:46 +0100

    Contraseña MongoDB

--- src/main/java/com/example/herokudemo/AppConfig.java
+++ src/main/java/com/example/herokudemo/AppConfig.java
@@ -10,10 +10,10 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 public class AppConfig {
 
   public @Bean MongoClient mongoClient() {
-      return new MongoClient("mongodb://heroku_92g8hdl7:7CX3ZWs2927jc5n@ds135413.mlab.com:35413");
+      return new MongoClient("mongodb://heroku_92g8hdl7:7CX3ZWs2927jc5n@ds135413.mlab.com:35413/heroku_92g8hdl7");
   }
 
   public @Bean MongoTemplate mongoTemplate() {
-      return new MongoTemplate(mongoClient(), "heroku_92g8hdl7");
+      return new MongoTemplate(mongoClient(), "canallaDB");
   }
 }
