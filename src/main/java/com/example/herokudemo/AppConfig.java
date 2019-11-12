b3e8b888c7936bc4055259408592599a43539e62
jvquiles <jvquiles@gmail.com>
2019-11-12 22:40:19 +0100

    Settings

--- src/main/java/com/example/herokudemo/AppConfig.java
+++ src/main/java/com/example/herokudemo/AppConfig.java
@@ -1,12 +1,6 @@
 package com.example.herokudemo;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class AppConfig {
