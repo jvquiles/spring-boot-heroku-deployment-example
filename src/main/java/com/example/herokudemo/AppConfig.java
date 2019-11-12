52008555b51271e92c14b0987232edd48d91cf6a
jvquiles <jvquiles@gmail.com>
2019-11-12 21:09:36 +0100

    Credenciales mongoDB

--- src/main/java/com/example/herokudemo/AppConfig.java
+++ src/main/java/com/example/herokudemo/AppConfig.java
@@ -1,6 +1,10 @@
 package com.example.herokudemo;
 
+import java.util.ArrayList;
+import java.util.List;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +14,14 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 public class AppConfig {
 
   public @Bean MongoClient mongoClient() {
+    List<MongoCredential> allCred = new ArrayList<MongoCredential>();
+    MongoCredential mongoCredential = MongoCredential.createCredential("heroku_92g8hdl7", "heroku_92g8hdl7", "7CX3ZWs2927jc5n".toCharArray());
+    ServerAddress serverAddress = new ServerAddress("ds135413.mlab.com", 35413);
+		MongoClient client = new MongoClient(serverAddress, mongoCredential, MongoClientOptions.builder().build());
+		client.setWriteConcern(WriteConcern.ACKNOWLEDGED);
+
+		return client;
+
       return new MongoClient("mongodb://heroku_92g8hdl7:7CX3ZWs2927jc5n@ds135413.mlab.com:35413/heroku_92g8hdl7");
   }
 
